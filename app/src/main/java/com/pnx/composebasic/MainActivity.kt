1381f10f5a49e42ec2eb2ac274079d1ad4d9cc73
SangDon <tkdehs0454@naver.com>
2022-06-02 20:08:46 +0900

    Compose 연습
    연습

--- app/src/main/java/com/pnx/composebasic/MainActivity.kt
+++ app/src/main/java/com/pnx/composebasic/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.pnx.composebasic
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pnx.composebasic.ui.theme.ComposeBasicTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,11 +33,59 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// 뷰
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Pnx Compose App")}
+                , backgroundColor = Color(0xfff25287))
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*do something*/}) {
+                Text("클릭")
+            }
+        }
+    ) {
+        MyComposableView()
+    }
+}
+@Composable
+fun RowItem() {
+    // horizontal 리니어 레이아웃
+    Row(
+        Modifier
+            .padding(top = 10.dp, start = 10.dp, end = 10.dp)
+            .background(Color(0xffeaffd0))
+            .fillMaxWidth()
+    ) {
+        Text(text = "안녕하세영", Modifier.padding(end = 10.dp), color = Color.Red)
+        Text(text = "안녕하세영", Modifier.padding(end = 10.dp), color = Color.Green)
+        Text(text = "안녕하세영", Modifier.padding(end = 10.dp), color = Color.Blue)
+    }
 }
 
+@Composable
+fun MyComposableView() {
+    Log.d("TAG", "MyComposableView")
+
+
+    // vertical 리니어 레이아웃
+    Column(
+        Modifier
+            .background(Color(0xffF6FBF4))
+            .verticalScroll(rememberScrollState())
+
+    ) {
+        for( i in 0..30){
+            RowItem()
+        }
+    }
+
+}
+
+// 미리보기
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
